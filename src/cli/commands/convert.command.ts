f363bf37c45b6b8847fc09e85a0b85ea12d33ba3
Junior Martins <juniormartinxo@gmail.com>
2022-09-01 09:28:22 -0300

    Convert command created

---
+++ src/cli/commands/convert.command.ts
@@ -0,0 +1,17 @@
+import { Command, CommandRunner } from 'nest-commander'
+import { ConverterService } from 'src/converter/converter.service'
+
+@Command({
+  name: 'convert',
+  arguments: '<origin> <destination>',
+  options: { isDefault: true },
+})
+export class ConvertCommand extends CommandRunner {
+  constructor(private readonly converterService: ConverterService) {
+    super()
+  }
+
+  async run(inputs: string[]): Promise<void> {
+    await this.converterService.getLog(inputs[0], inputs[1])
+  }
+}
