c94fc1ab546e1831f2d4f7fdd0c6dae1b8b30405
Junior Martins <juniormartinxo@gmail.com>
2022-09-01 09:29:38 -0300

    Captor service created

---
+++ src/captor/captor.module.ts
@@ -0,0 +1,10 @@
+import { HttpModule } from '@nestjs/axios'
+import { Module } from '@nestjs/common'
+import { CaptorService } from './captor.service'
+
+@Module({
+  imports: [HttpModule],
+  controllers: [],
+  providers: [CaptorService],
+})
+export class CaptorModule {}
